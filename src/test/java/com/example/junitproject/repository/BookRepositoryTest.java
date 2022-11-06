232abd042a12339bf7b857a11e5758771b74476d
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2022-11-06 15:17:06 +0900

    test : add book save test
    given : Book info(title, author)
    when : save return persistent Book
    then : Book.equals(persistentBook)

--- src/test/java/com/example/junitproject/repository/BookRepositoryTest.java
+++ src/test/java/com/example/junitproject/repository/BookRepositoryTest.java
@@ -1,9 +1,12 @@
 package com.example.junitproject.repository;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.example.junitproject.domain.Book;
 
 /*
  * @DataJpaTest : DB와 관련된 컴포넌트만 메모리에 로딩
@@ -19,10 +22,21 @@ public class BookRepositoryTest {
 	@Test
 	void givenBookInfo_whenSave_thenNotThrow() {
 		//given
+		String title = "junit5";
+		String author = "sjk";
+		Book book = Book.builder()
+			.title(title)
+			.author(author)
+			.build();
 
 		//when
+		Book persistentBook = bookRepository.save(book);
 
 		//then
+		Assertions.assertEquals(title, persistentBook.getTitle());
+		Assertions.assertEquals(author, persistentBook.getAuthor());
+		Assertions.assertEquals(book, persistentBook);
 
 	}
+
 }
