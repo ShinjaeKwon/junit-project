a58a43c86b1390ca262bcc99cc6fe083a96b8c96
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2022-11-06 17:30:43 +0900

    feat : add registerBook

--- src/main/java/com/example/junitproject/service/BookService.java
+++ src/main/java/com/example/junitproject/service/BookService.java
@@ -1,4 +1,23 @@
 package com.example.junitproject.service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.junitproject.controller.dto.request.BookSaveRequest;
+import com.example.junitproject.controller.dto.response.BookResponse;
+import com.example.junitproject.repository.BookRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
 public class BookService {
+
+	private final BookRepository bookRepository;
+
+	@Transactional(rollbackFor = RuntimeException.class)
+	public BookResponse registerBook(BookSaveRequest request) {
+		return new BookResponse().from(bookRepository.save(request.toEntity()));
+	}
+
 }
