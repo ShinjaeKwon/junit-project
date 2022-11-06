217b061ddc57c19920fe1acca3ffc27697f46c24
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2022-11-06 17:29:28 +0900

    feat : add save request dto

---
+++ src/main/java/com/example/junitproject/controller/dto/request/BookSaveRequest.java
@@ -0,0 +1,26 @@
+package com.example.junitproject.controller.dto.request;
+
+import com.example.junitproject.domain.Book;
+
+import lombok.AllArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@AllArgsConstructor
+public class BookSaveRequest {
+
+	private String title;
+	private String author;
+
+	public static BookSaveRequest of(String title, String author) {
+		return new BookSaveRequest(title, author);
+	}
+
+	public Book toEntity() {
+		return Book.builder()
+			.title(this.title)
+			.author(this.author)
+			.build();
+	}
+
+}
