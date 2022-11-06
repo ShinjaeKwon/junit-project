3a48d39923968f870e8ec27d5386019bfa16447b
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2022-11-06 15:00:35 +0900

    test : setup BookRepositoryTest
    * @DataJpaTest : DB와 관련된 컴포넌트만 메모리에 로딩
    * @Autowired : Dependency Injection By Test
    
    테스트 종류
    * 책 등록 테스트
    * 책 목록보기 테스트
    * 책 한권보기 테스트
    * 책 수정 테스트
    * 책 삭제 테스트

--- src/test/java/com/example/junitproject/repository/BookRepositoryTest.java
+++ src/test/java/com/example/junitproject/repository/BookRepositoryTest.java
@@ -1,4 +1,28 @@
 package com.example.junitproject.repository;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+/*
+ * @DataJpaTest : DB와 관련된 컴포넌트만 메모리에 로딩
+ * @Autowired : Dependency Injection By Test
+ * */
+@DataJpaTest
 public class BookRepositoryTest {
+
+	@Autowired
+	private BookRepository bookRepository;
+
+	@DisplayName("책 등록 테스트")
+	@Test
+	void givenBookInfo_whenSave_thenNotThrow() {
+		//given
+
+		//when
+
+		//then
+
+	}
 }
