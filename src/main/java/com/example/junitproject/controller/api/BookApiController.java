10f50fd38a93cf241c23640b74bed997c270a753
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2022-11-07 16:32:03 +0900

    feat : add book save v1
    validation 단에서 에러 잡힐 시 에러 처리 까지 추가
    하지만 JSON 파싱 단계에서 에러가 발생하면 잡지 못한다. -> Exception Handler 를 통해 잡아야 한다.

--- src/main/java/com/example/junitproject/controller/api/BookApiController.java
+++ src/main/java/com/example/junitproject/controller/api/BookApiController.java
@@ -1,8 +1,53 @@
 package com.example.junitproject.controller.api;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.junitproject.controller.dto.request.BookSaveRequest;
+import com.example.junitproject.controller.dto.response.Response;
+import com.example.junitproject.service.BookService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @RestController
 public class BookApiController {
+
+	private final BookService bookService;
+
+	@PostMapping("/api/v1/book")
+	public ResponseEntity<?> saveBook(@RequestBody @Valid BookSaveRequest request, BindingResult bindingResult) {
+
+		if (bindingResult.hasErrors()) {
+			return new ResponseEntity<>(Response.builder()
+				.resultCode("error")
+				.message(getErrors(bindingResult).toString())
+				.body(request)
+				.build(), HttpStatus.BAD_REQUEST);
+		}
+		return new ResponseEntity<>(Response.builder()
+			.resultCode("success")
+			.message("글 저장 성공")
+			.body(bookService.registerBook(request))
+			.build(), HttpStatus.CREATED);
+	}
+
+	private static Map<String, String> getErrors(BindingResult bindingResult) {
+		Map<String, String> errorMap = new HashMap<>();
+		for (FieldError fieldError : bindingResult.getFieldErrors()) {
+			errorMap.put(fieldError.getField(), fieldError.getDefaultMessage());
+		}
+		return errorMap;
+	}
 
 }
