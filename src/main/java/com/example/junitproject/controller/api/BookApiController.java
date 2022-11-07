4be49136cdae3221d8cb115f89fe8da5bc99edc2
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2022-11-07 17:45:37 +0900

    feat : add delete book
    ResponseEntity 에서 응답 값이 없어지기 때문에 HttpStatus.NO_CONTENT -> OK 로 변경

--- src/main/java/com/example/junitproject/controller/api/BookApiController.java
+++ src/main/java/com/example/junitproject/controller/api/BookApiController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -45,6 +46,13 @@ public class BookApiController {
 	@GetMapping("/api/v1/book/{id}")
 	public ResponseEntity<?> getBookOne(@PathVariable Long id) {
 		return new ResponseEntity<>(getSuccessResponse("책 한건 불러오기 성공", bookService.getBook(id)),
+			HttpStatus.OK);
+	}
+
+	@DeleteMapping("/api/v1/book/{id}")
+	public ResponseEntity<?> deleteBook(@PathVariable Long id) {
+		bookService.removeBook(id);
+		return new ResponseEntity<>(getSuccessResponse("책 삭제하기 성공", null),
 			HttpStatus.OK);
 	}
 
